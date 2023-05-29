12793a8da65fb9a421319ab7e2608721146b3c61
onewaykaranja <mkaranjapatrick@gmail.com>
2023-05-29 10:56:34 +0300

    Bash script written and given appropriate permission. Created a log file

---
+++ ipblock.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+LOG_FILE="/Desktop/githubportfolio/bash/ipblock/blockips.log"
+BLOCKED_IPS_FILE="/etc/blocked_ips.txt"
+MAX_FAILED_ATTEMPTS=3
+
+# Start an infinite loop for continuous monitoring
+while true
+do
+
+    if [[ $(grep -c "<IP_to_check>" $LOG_FILE) -ge $MAX_FAILED_ATTEMPTS ]]; then
+        IP_TO_BLOCK="<IP_to_block>"
+        # Check and avoid duplicate entries
+        if ! grep -q "^$IP_TO_BLOCK$" "$BLOCKED_IPS_FILE"; then
+            echo "$(date '+%Y-%m-%d %H:%M:%S') - Blocking IP: $IP_TO_BLOCK" >> $LOG_FILE
+            echo $IP_TO_BLOCK >> $BLOCKED_IPS_FILE
+
+        fi
+    fi
+
+    # Sleep for 10 secs before checking again
+    sleep 10
+done
